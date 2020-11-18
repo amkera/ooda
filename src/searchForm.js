0351e4c13ad28a40b83df63a4538b32d9e89d86c
amkera <60901491+amkera@users.noreply.github.com>
2020-11-17 19:27:26 -0500

    got local state working

--- src/searchForm.js
+++ src/searchForm.js
@@ -1,54 +1,61 @@
 import React from 'react';
 
 class SearchForm extends React.Component {
-
-  // const API = 'https://npiregistry.cms.hhs.gov/api/?version=2.1'
-
+  // const api = 'https://npiregistry.cms.hhs.gov/api/?version=2.1'
 
   state = { searchInput: '' }
 
   handleChange = (event) => {
+    debugger
     this.setState({
       [event.target.name]: event.target.value
     })
   }
 
+  handleFirstNameSubmit = (event) => {
+
+  }
+
   render() {
+
     return (
       <div>
 
-        <form>
+        <form >
           <label>Search by provider first name</label>
            <input
               type="text"
-              placeholder="Name"
-              name="name"
+              placeholder="First Name"
+              name="searchInput"
               value={this.state.searchInput}
+              onChange={this.handleChange}
             />
             <br/>
           <input type="submit" />
         </form>
 
-        <form>
+        <form onSubmit={this.handleLastNameSubmit}>
           <label>Search by provider last name</label>
 
             <input
               type="text"
-              placeholder="Name"
-              name="name"
+              placeholder="Last Name"
+              name="searchInput"
               value={this.state.searchInput}
+              onChange={this.handleChange}
             />
             <br/>
           <input type="submit" />
         </form>
 
-        <form>
+        <form onSubmit={this.handleOrgSubmit}>
           <label>Search by provider organization name</label>
             <input
               type="text"
-              placeholder="Name"
-              name="name"
+              placeholder="Organization Name"
+              name="searchInput"
               value={this.state.searchInput}
+              onChange={this.handleChange}
             />
             <br/>
           <input type="submit" />
